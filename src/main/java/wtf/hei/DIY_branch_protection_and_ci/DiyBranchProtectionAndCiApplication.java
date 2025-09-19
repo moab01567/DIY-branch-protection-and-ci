6571e9342facf9f57f17512743d5d7e36640d8dd
moab01567 <moab015@student.kristiania.no>
2025-09-19 14:30:54 +0200

    init

---
+++ src/main/java/wtf/hei/DIY_branch_protection_and_ci/DiyBranchProtectionAndCiApplication.java
@@ -0,0 +1,13 @@
+package wtf.hei.DIY_branch_protection_and_ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyBranchProtectionAndCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiyBranchProtectionAndCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wtf/hei/DIY_branch_protection_and_ci/DiyBranchProtectionAndCiApplicationTests.java
@@ -0,0 +1,13 @@
+package wtf.hei.DIY_branch_protection_and_ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiyBranchProtectionAndCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
